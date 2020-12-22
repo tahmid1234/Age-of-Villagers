60cd2b9b7fd9d8f861bacfbdf08650e5f2390163
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 15:25:39 +0600

    All Model Class Added

---
+++ AgeOfVillagers/AgeOfVillagers/Model Class Folder/State.cs
@@ -0,0 +1,19 @@
+﻿using AgeOfVillagers.Interface;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.Model_Class_Folder
+{
+    public class State
+    {
+       
+        
+
+        public string VillageName { get; set; }
+        public List<DrawnItemsInformation> DrawnItemsInformationList { get; set; }
+
+
+    }
+}
+
