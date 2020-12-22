400aecb8419060b22587473e7315e02b9151b1e4
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 15:16:50 +0600

    All Abstract Classes added

---
+++ AgeOfVillagers/AgeOfVillagers/AbstractClass/DrawableShapes.cs
@@ -0,0 +1,36 @@
+﻿using AgeOfVillagers.Interface;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers.AbstractClass
+{
+    public abstract class DrawableShapes : Shapes
+    {
+        public abstract void drawShape();
+
+        public void setDrawingElement( Graphics g, Pen p)
+        {
+            
+        }
+
+
+        internal DrawableShapes getLines(Point start, Point end,Graphics g,Pen pen)
+        {
+            return new Line(start, end,g,pen);
+        }
+
+        internal DrawableShapes getArcs(Graphics g, Pen pen, Point point, int startAng, int endAng, int lenght, int width)
+        {
+            
+            return new Arc(g,pen,point,startAng,endAng,lenght,width);
+        }
+
+        
+
+        public abstract void makeShape();
+        
+    }
+}
---
+++ AgeOfVillagers/AgeOfVillagers/AbstractClass/Environment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers
+{
+    public abstract class Environment
+    {
+        public abstract void showNationName();
+        public abstract void ValidateOtherGameElements();
+
+        public abstract void setTerrainColor();
+
+        public void setEnvironment()
+        {
+            showNationName();
+            ValidateOtherGameElements();
+            setTerrainColor();
+        }
+
+
+
+    }
+}
