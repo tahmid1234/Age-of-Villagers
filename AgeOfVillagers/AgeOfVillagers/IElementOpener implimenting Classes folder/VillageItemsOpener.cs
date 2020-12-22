468945ad64a2232924fab7b24dfdaa7726eca2f5
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 15:23:29 +0600

    All IElementOpener implimenting Classes added

---
+++ AgeOfVillagers/AgeOfVillagers/IElementOpener implimenting Classes folder/VillageItemsOpener.cs
@@ -0,0 +1,38 @@
+﻿using AgeOfVillagers.FactoryClasses;
+using AgeOfVillagers.Interface;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Text;
+
+namespace AgeOfVillagers.IStateElements_implimentinf_folder
+{
+    class VillageItemsOpener : IElementOpener
+    {
+        List<DrawnItemsInformation> drawnItemsInfoList;
+        private string selectedNation;
+        
+        private Graphics graphics;
+        private Pen pen;
+        ItemFactory itemFactory;
+        IItem item;
+
+        public VillageItemsOpener(List<DrawnItemsInformation> drawnItemsInfoList, string selectedNation, Graphics graphics, Pen pen)
+        {
+            this.drawnItemsInfoList = drawnItemsInfoList;
+            this.selectedNation = selectedNation;
+            this.graphics = graphics;
+            this.pen = pen;
+            itemFactory = new ItemFactory();
+        }
+
+        public void displayElements()
+        {
+            foreach(DrawnItemsInformation drawnItemsInformation in drawnItemsInfoList)
+            {
+                item = itemFactory.GetItem(drawnItemsInformation.Clicked_point, drawnItemsInformation.Item_type, graphics, pen);
+                item.placeItem(selectedNation);
+            }
+        }
+    }
+}
---
+++ AgeOfVillagers/AgeOfVillagers/IElementOpener implimenting Classes folder/VillageNameOpener.cs
@@ -0,0 +1,29 @@
+﻿using AgeOfVillagers.Interface;
+using AgeOfVillagers.Model_Class_Folder;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Forms;
+
+namespace AgeOfVillagers.IStateElements_implimentinf_folder
+{
+    class VillageNameOpener : IElementOpener
+    {
+        private Label villageNameLabel;
+        private string villageName;
+        private State previosulySavedState;
+
+        public VillageNameOpener(Label villageNameLabel, string villageName)
+        {
+            this.villageNameLabel = villageNameLabel;
+            this.villageName = villageName;
+        }
+
+       
+
+        public void displayElements()
+        {
+            villageNameLabel.Text = villageName;
+        }
+    }
+}
