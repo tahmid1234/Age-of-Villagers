dac470a09397389f9f5fedb439b84e6122291e67
tahmid1234 <tahmidislam1998@gmail.com>
2020-12-22 15:25:08 +0600

    All JSON Handling Classes Added

---
+++ AgeOfVillagers/AgeOfVillagers/JSON Handling Classes Folder/JsonConversion.cs
@@ -0,0 +1,34 @@
+﻿using AgeOfVillagers.Model_Class_Folder;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AgeOfVillagers.JSON_Handling_Classes_Folder
+{
+    public class JsonConversion
+    {
+        String jsonData;
+        public string serialize(State currentState)
+        {
+            
+                var dataString = JsonConvert.SerializeObject(currentState, Formatting.Indented);
+                return dataString;
+                
+            
+        }
+
+        public State deserialize(string jsonData)
+        {
+            var settings = new JsonSerializerSettings()
+            {
+                TypeNameHandling = TypeNameHandling.All
+            };
+
+
+
+            State  previouslySavedState = JsonConvert.DeserializeObject<State>(jsonData, settings);
+            return previouslySavedState;
+        }
+    }
+}
